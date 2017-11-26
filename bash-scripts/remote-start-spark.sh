6e55ae93b1d619c3ab7aa7f0da63a5ecf38081ea
weiren2 <weiren2@illinois.edu>
2017-11-26 14:45:04 -0600

    start all nodes as workers

--- bash-scripts/remote-start-spark.sh
+++ bash-scripts/remote-start-spark.sh
@@ -4,9 +4,8 @@ for (( i = 1; i < 11; i++ )); do
     var=$(printf "weiren2@fa17-cs425-g28-%02d.cs.illinois.edu" $i)
     if [[ $i -eq 1 ]] ; then
         ssh $var 'export PATH=/usr/local/go/bin:$PATH; cd ~/go/src/cs425_mp4; git pull; bash bash-scripts/spark-start-master.sh'
-    else
-        ssh $var 'export PATH=/usr/local/go/bin:$PATH; cd ~/go/src/cs425_mp4; git pull; bash bash-scripts/spark-start-slave.sh' &
     fi
+    ssh $var 'export PATH=/usr/local/go/bin:$PATH; cd ~/go/src/cs425_mp4; git pull; bash bash-scripts/spark-start-slave.sh' &
 done
 
 wait

a6ce9c0caa53c9683b05ee840fbc7ec65174aa69
weiren2 <weiren2@illinois.edu>
2017-11-26 14:33:36 -0600

    added script to stop spark

---
+++ bash-scripts/remote-stop-spark.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+for (( i = 1; i < 11; i++ )); do
+    var=$(printf "weiren2@fa17-cs425-g28-%02d.cs.illinois.edu" $i)
+    ssh $var 'export PATH=/usr/local/go/bin:$PATH; cd ~/go/src/cs425_mp4; git pull; bash bash-scripts/spark-stop.sh' &
+done
+wait

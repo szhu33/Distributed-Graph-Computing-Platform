1863bf88f6b7eadc661440365a9cea7f5fa5781a
weiren2 <weiren2@illinois.edu>
2017-11-26 14:40:10 -0600

    use exit instead of return in the scripts

--- bash-scripts/spark-deploy.sh
+++ bash-scripts/spark-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-cd $HOME || return
+cd $HOME || exit 1
 wget http://apache.mirrors.ionfish.org/spark/spark-2.2.0/spark-2.2.0-bin-hadoop2.7.tgz
 tar -xf spark-2.2.0-bin-hadoop2.7.tgz

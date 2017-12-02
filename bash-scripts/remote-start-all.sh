5235e24ceedca0ceb108b053253ec9f560052e9b
Wei <6765243+victor-ren@users.noreply.github.com>
2017-12-01 20:23:44 -0600

    directly start!

--- bash-scripts/remote-start-all.sh
+++ bash-scripts/remote-start-all.sh
@@ -3,11 +3,11 @@
 for (( i = 1; i < 11; i++ )); do
     var=$(printf "weiren2@fa17-cs425-g28-%02d.cs.illinois.edu" $i)
     if [[ $i -eq 10 ]]; then
-        konsole --new-tab -e ssh -t $var 'export PATH=/usr/local/go/bin:$PATH; cd ~/go/src/cs425_mp4; git pull; cd master; go build; bash -l' &
+        konsole --new-tab -e ssh -t $var 'export PATH=/usr/local/go/bin:$PATH; cd ~/go/src/cs425_mp4; git pull; cd master; go build; ./master' &
     elif [[ $i -eq 7 ]]; then
-        konsole --new-tab -e ssh -t $var 'export PATH=/usr/local/go/bin:$PATH; cd ~/go/src/cs425_mp4; git pull; cd client; go build; bash -l' &
+        konsole --new-tab -e ssh -t $var 'export PATH=/usr/local/go/bin:$PATH; cd ~/go/src/cs425_mp4; git pull; cd client; go build; ./client' &
     else
-        konsole --new-tab -e ssh -t $var 'export PATH=/usr/local/go/bin:$PATH; cd ~/go/src/cs425_mp4; git pull; cd worker; go build; bash -l' &
+        konsole --new-tab -e ssh -t $var 'export PATH=/usr/local/go/bin:$PATH; cd ~/go/src/cs425_mp4; git pull; cd worker; go build; ./worker' &
     fi
 
     sleep 0.01s

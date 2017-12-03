a565292270f1b726ca1ca3d1a04aa6868a07ee92
Wei <6765243+victor-ren@users.noreply.github.com>
2017-12-03 13:28:14 -0600

    use 10 as server

--- bash-scripts/remote-start-spark.sh
+++ bash-scripts/remote-start-spark.sh
@@ -2,10 +2,13 @@
 
 for (( i = 1; i < 11; i++ )); do
     var=$(printf "weiren2@fa17-cs425-g28-%02d.cs.illinois.edu" $i)
-    if [[ $i -eq 1 ]] ; then
+    if [[ $i -eq 10 ]] ; then
         ssh $var 'export PATH=/usr/local/go/bin:$PATH; cd ~/go/src/cs425_mp4; git pull; bash bash-scripts/spark-start-master.sh'
+    elif [[ $i -eq 5 || $i -eq 9 ]] ; then
+        continue
+    else
+        ssh $var 'export PATH=/usr/local/go/bin:$PATH; cd ~/go/src/cs425_mp4; git pull; bash bash-scripts/spark-start-slave.sh' &
     fi
-    ssh $var 'export PATH=/usr/local/go/bin:$PATH; cd ~/go/src/cs425_mp4; git pull; bash bash-scripts/spark-start-slave.sh' &
 done
 
 wait
